e071c74d47d39cd4a76b1c321ee36fd242cb01c1
Patryk Żmigrodzki <pzmi@users.noreply.github.com>
2022-01-11 20:28:58 +0100

    Add basic setup script

---
+++ setup.sh
@@ -0,0 +1,4 @@
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew bundle
